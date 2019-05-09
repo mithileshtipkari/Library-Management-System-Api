b89634669eade72442cfe66d50a130bb44c4d784
krutikasawarkar <krutikasawarkar21@gmail.com>
2019-05-09 12:15:42 +0530

    Initial change for Data Access layer

---
+++ DataAccess/DBConfiguration.cs
@@ -0,0 +1,9 @@
+﻿namespace DataAccess
+{
+    public class DBConfiguration
+    {
+        public string Address { get; set; }
+
+        public  string Port { get; set; }
+    }
+}
---
+++ DataAccess/LibraryManagementDBContext.cs
@@ -0,0 +1,28 @@
+﻿using MongoDB.Driver;
+
+namespace DataAccess
+{
+    public class LibraryManagementDBContext
+    {
+        private readonly IMongoDatabase _database = null;
+
+        #region MyRegion
+
+        private static readonly LibraryManagementDBContext _instance = new LibraryManagementDBContext();
+
+        public static LibraryManagementDBContext Instance
+        {
+            get
+            {
+                return _instance;
+            }
+        }
+
+        public LibraryManagementDBContext()
+        {
+            var connectionString = "";
+        }
+        #endregion
+
+    }
+}
