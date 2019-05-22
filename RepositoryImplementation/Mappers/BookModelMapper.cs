9b11c27e5094e577716a996509d775d32b2ceb51
krutikasawarkar <krutikasawarkar21@gmail.com>
2019-05-22 10:59:16 +0530

    Replaced auto mapper with manual mapping.

--- RepositoryImplementation/Mappers/BookModelMapper.cs
+++ RepositoryImplementation/Mappers/BookModelMapper.cs
@@ -1,7 +1,9 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using AutoMapper;
 using DataAccess.Entity_Model;
 using DomainModel;
+using DomainModel.Enums;
 
 namespace RepositoryImplementation.Mappers
 {
@@ -9,12 +11,15 @@ namespace RepositoryImplementation.Mappers
     {
         public static List<BookItem> GetBookDomainModels(List<BookItemEntityModel> bookItemEntityModels)
         {
-            MapperConfiguration mapperConfiguration = new MapperConfiguration(config =>
-                    config.CreateMap<BookItemEntityModel, BookItem>()
-                );
+            List<BookItem> bookItems = new List<BookItem>();
 
-            IMapper mapper = mapperConfiguration.CreateMapper();
-            return mapper.Map<List<BookItemEntityModel>, List<BookItem>>(bookItemEntityModels);
+            foreach (BookItemEntityModel bookItemEntityModel in bookItemEntityModels)
+            {
+                BookItem bookItem = GetBookDomainModel(bookItemEntityModel);
+                bookItems.Add(bookItem);
+            }
+
+            return bookItems;
         }
 
         public static BookItem GetBookDomainModel(BookItemEntityModel bookItemEntityModel)
@@ -24,8 +29,8 @@ namespace RepositoryImplementation.Mappers
                                                    bookItemEntityModel.Borrowed,
                                                    bookItemEntityModel.DueDate,
                                                    bookItemEntityModel.Price,
-                                                   bookItemEntityModel.Format,
-                                                   bookItemEntityModel.Status,
+                                                   ConvertStringToEnumBookFormat(bookItemEntityModel.Format),
+                                                   ConvertStringToEnumBookStatus(bookItemEntityModel.Status),
                                                    bookItemEntityModel.DateOfPurchase,
                                                    bookItemEntityModel.PublicationDate,
                                                    bookItemEntityModel.PlacedAt,
@@ -33,9 +38,69 @@ namespace RepositoryImplementation.Mappers
                                                    bookItemEntityModel.Title,
                                                    bookItemEntityModel.Subject,
                                                    bookItemEntityModel.Publisher,
-                                                   bookItemEntityModel.Language,
+                                                   ConvertStringToEnumLanguage(bookItemEntityModel.Language),
                                                    bookItemEntityModel.NoOfPages,
-                                                   bookItemEntityModel.Authors);          
+                                                   bookItemEntityModel.Authors);
+            return bookItemDomainModel;
+        }
+
+        private static Language ConvertStringToEnumLanguage(string language)
+        {
+            switch (language)
+            {
+                case "Marathi":
+                    return Language.Marathi;
+                case "Hindi":
+                    return Language.Hindi;
+                case "English":
+                    return Language.English;
+                case "Urdu":
+                    return Language.Urdu;
+                case "Sanskrit":
+                    return Language.Sanskrit;
+            }
+
+            return Language.Unknown;
+        }
+
+        private static BookStatus ConvertStringToEnumBookStatus(string status)
+        {
+            switch (status)
+            {
+                case "Available":
+                    return BookStatus.Available;
+                case "Reserved":
+                    return BookStatus.Reserved;
+                case "Loaned":
+                    return BookStatus.Loaned;
+                case "Lost":
+                    return BookStatus.Lost;
+            }
+
+            return BookStatus.Unknown;
+        }
+
+        private static BookFormat ConvertStringToEnumBookFormat(string format)
+        {
+            switch (format)
+            {
+                case "Hardcover":
+                    return BookFormat.Hardcover;
+                case "Paperback":
+                    return BookFormat.Paperback;
+                case "AudioBook":
+                    return BookFormat.AudioBook;
+                case "Ebook":
+                    return BookFormat.Ebook;
+                case "Newspaper":
+                    return BookFormat.Newspaper;
+                case "Magazine":
+                    return BookFormat.Magazine;
+                case "Journal":
+                    return BookFormat.Journal;
+            }
+
+            return BookFormat.Unknown;
         }
 
         public static BookItemEntityModel GetBookItemEntityModel(BookItem bookItem)
