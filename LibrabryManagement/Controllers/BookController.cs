2d7e53f553a055d84136fd3ea2db0248e4358bb8
krutikasawarkar <krutikasawarkar21@gmail.com>
2019-05-25 22:44:28 +0530

    Injected MongoRepository in Api layer and renamed Api project.

--- LibrabryManagement/Controllers/BookController.cs
+++ LibrabryManagement/Controllers/BookController.cs
@@ -4,11 +4,11 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 
-namespace LibrabryManagement.Controllers
+namespace LibraryManagementApi.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class ValuesController : ControllerBase
+    public class BookController : ControllerBase
     {
         // GET api/values
         [HttpGet]
--- LibrabryManagement/Startup.cs
+++ LibrabryManagement/Startup.cs
@@ -9,8 +9,10 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using RepositoryImplementation;
+using UseCaseBoundary;
 
-namespace LibrabryManagement
+namespace LibraryManagementApi
 {
     public class Startup
     {
@@ -25,6 +27,7 @@ namespace LibrabryManagement
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services.AddScoped<BookRepository, BookMongoRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
